8a0f9fc094067b130014a541808dd466e4b2c664
goldman220 <2891429069@qq.com>
2024-03-18 00:07:07 +0800

    修改数据处理为buffer

--- src/main/kotlin/SummerBootServerMain/SummerBootServer.kt
+++ src/main/kotlin/SummerBootServerMain/SummerBootServer.kt
@@ -1,8 +1,8 @@
 package SummerBootServerMain
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.StringReader
 import java.net.ServerSocket
 import java.net.Socket
 
@@ -13,7 +13,6 @@ fun main(args: Array<String>) {
     while (true){
         //等待客户端socket连接
         val socket = serviceSocket.accept()
-        println("客户端上线"+socket.localAddress.hostAddress)
         println("客户端上线"+socket.inetAddress.hostAddress)
 
         //开启携程处理链接内容
@@ -30,9 +29,13 @@ fun main(args: Array<String>) {
 }
 //连接处理
 suspend fun handleSocket(socket: Socket){
+    val bufferReader = BufferedReader(InputStreamReader(socket.getInputStream()))
     while (true){
-        var inputStream = socket.getInputStream();
-        println(inputStream.read())
         println(">>>")
+        delay(1000 )
+        while (bufferReader.ready()){
+            println("---")
+            println(bufferReader.readText())
+        }
     }
 }
